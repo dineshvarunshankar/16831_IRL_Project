c993bdb262873fc10261a9e905b687e189941530
Vyvaswath <113670456+vyvas33@users.noreply.github.com>
2026-03-17 01:22:55 -0400

    Add SAC implementation, training and project structure

---
+++ train/agents/sac/actor.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Normal
+
+LOG_STD_MAX = 2.0
+LOG_STD_MIN = -20.0
+
+class Actor(nn.Module):
+    def __init__(self, obs_dim, act_dim, hidden_dim = 256):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(obs_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU()
+        )
+
+        self.mean_head = nn.Linear(hidden_dim, act_dim)
+        self.log_std_head = nn.Linear(hidden_dim, act_dim)
+
+
+    def forward(self, state):
+        x = self.net(state)
+        mean = self.mean_head(x)
+        log_std = self.log_std_head(x)
+
+        log_std = torch.clamp(log_std, LOG_STD_MIN, LOG_STD_MAX)
+
+        return mean, log_std
+
+
+    def sample(self, state):
+        mean, log_std = self.forward(state)
+        std = log_std.exp()
+
+        dist = Normal(mean, std)
+        u = dist.rsample()
+
+        action = torch.tanh(u) # to squash to (-1,1)
+
+        log_prob = dist.log_prob(u) - torch.log(1 - action.pow(2) + 1e-6)
+        log_prob = log_prob.sum(dim=-1, keepdim=True)
+
+        return action, log_prob
---
+++ train/configs/config_loader.py
@@ -0,0 +1,25 @@
+import yaml
+from dataclasses import dataclass
+
+@dataclass
+class SACConfig:
+    motion_path       : str
+    actor_hidden_dim  : int
+    critic_hidden_dim : int
+    lr                : float
+    gamma             : float
+    tau               : float
+    batch_size        : int
+    buffer_size       : int
+    total_steps       : int
+    learning_starts   : int
+    log_freq          : int
+    save_freq         : int
+    device            : str = 'cpu'
+
+def load_config(path):
+    import torch
+    with open(path) as f:
+        cfg = yaml.safe_load(f)
+    cfg['device'] = 'cuda' if torch.cuda.is_available() else 'cpu'
+    return SACConfig(**cfg)
